c1a23576e4baea5976874938d33ff45247bee573
Farah <aiwithfarah@gmail.com>
2026-07-08 00:12:40 +0530

    Add files via upload

---
+++ BroCode_Full_Python_Course/conditional_expressions/ternary_max_num.py
@@ -0,0 +1,6 @@
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+result = f"{a} is greater than {b}" if a > b else f"{b} is greater than {a}"
+
+print(result)
---
+++ BroCode_Full_Python_Course/conditional_expressions/ternary_operator.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+
+# Check if the number is positive or negative
+print("Positive" if num > 0 else "Negative" )
